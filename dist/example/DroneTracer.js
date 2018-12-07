244db8115d6d0002ed570f385539b6bc1847a5f1
action-script <mail.actionscript@gmail.com>
2018-12-07 10:09:34 +0100

    DroneTracer | develop - removing dist from version control

--- dist/example/DroneTracer.js
+++
@@ -1,1 +0,0 @@
-../DroneTracer.js
--- dist/example/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# link library inside server range
-rm -f DroneTracer.js && ln -s ../DroneTracer.js .
-
-
-# start simple server
-python3 -m http.server 8000
